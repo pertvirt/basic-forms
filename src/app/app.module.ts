127a55362c9e7290bef041c70e2c3c5565d71238
pertvirt <justforplaydota2@gmail.com>
2020-04-17 13:52:19 +0700

    add demo-form-with-events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import { DemoFormSkuWithBuilderComponent } from './demo-form-sku-with-builder/demo-form-sku-with-builder.component';
 import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
 import { DemoFormWithCustomValidationComponent } from './demo-form-with-custom-validation/demo-form-with-custom-validation.component';
+import { DemoFormWithEventsComponent } from './demo-form-with-events/demo-form-with-events.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DemoFormWithCustomValidationComponent } from './demo-form-with-custom-v
     DemoFormSkuComponent,
     DemoFormSkuWithBuilderComponent,
     DemoFormWithValidationsExplicitComponent,
-    DemoFormWithCustomValidationComponent
+    DemoFormWithCustomValidationComponent,
+    DemoFormWithEventsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.component.ts
+++ src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.component.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, FormControl, Validators, AbstractControl} from '@angular/forms';
+
+
+/**
+ * Our custom validator
+ *
+ * A validator:
+ * - Takes a `Control` as it's input and
+ * - Returns a `StringMap<string, boolean>` where the key is "error code" and
+ *   the value is `true` if it fails
+ */
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return {invalidSku: true};
+  }
+}
 
 @Component({
   selector: 'app-demo-form-with-custom-validation',
@@ -6,10 +22,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo-form-with-custom-validation.component.css']
 })
 export class DemoFormWithCustomValidationComponent implements OnInit {
+  myForm: FormGroup;
+  sku: AbstractControl;
 
-  constructor() { }
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      'sku': ['', Validators.compose([
+        Validators.required, skuValidator
+      ])]
+    });
+    this.sku = this.myForm.controls['sku'];
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(value: string): void {
+    console.log('you submitted value:', value);
   }
 
 }
